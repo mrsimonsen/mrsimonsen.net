e5c415fc859272dab3edfd32305db4d8d00061de
John Simonsen <jsimo60@wgu.edu>
2025-08-04 10:06:56 -0600

    added qa_bot interface

--- simoscript.js
+++ simoscript.js
@@ -1,38 +1,36 @@
 //navDrop
-function navDrop(id1){
-  var content = document.getElementById(id1);
-  if(content.className.indexOf("w3-show")== -1){
-    content.className += " w3-show";
-  } else{
-    content.className = content.className.replace(" w3-show", "");
-  }
+function navDrop(id1) {
+	var content = document.getElementById(id1);
+	if (content.className.indexOf("w3-show") == -1) {
+		content.className += " w3-show";
+	} else {
+		content.className = content.className.replace(" w3-show", "");
+	}
 }
 // Side navigation
 function w3_open() {
-  var x = document.getElementById("mySidebar");
-  x.style.width = "100%";
-  x.style.fontSize = "40px";
-  x.style.paddingTop = "10%";
-  x.style.display = "block";
+	var x = document.getElementById("mySidebar");
+	x.style.width = "100%";
+	x.style.fontSize = "40px";
+	x.style.paddingTop = "10%";
+	x.style.display = "block";
 }
 function w3_close() {
-  document.getElementById("mySidebar").style.display = "none";
+	document.getElementById("mySidebar").style.display = "none";
 }
 // Tabs
 function openClass(evt, course) {
-  var i;
-  var x = document.getElementsByClassName("course");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  var activebtn = document.getElementsByClassName("testbtn");
-  for (i = 0; i < activebtn.length; i++){
-    activebtn.item(i).className = activebtn.item(i).className.replace(" w3-black", "");
-  }
+	var i;
+	var x = document.getElementsByClassName("course");
+	for (i = 0; i < x.length; i++) {
+		x[i].style.display = "none";
+	}
+	var activebtn = document.getElementsByClassName("testbtn");
+	for (i = 0; i < activebtn.length; i++) {
+		activebtn.item(i).className = activebtn.item(i).className.replace(" w3-black", "");
+	}
 
-  document.getElementById(course).style.display = "block";
-  evt.currentTarget.className += " w3-black";
+	document.getElementById(course).style.display = "block";
+	evt.currentTarget.className += " w3-black";
 }
 
-var mybtn = document.getElementsByClassName("testbtn")[0];
-mybtn.click();
